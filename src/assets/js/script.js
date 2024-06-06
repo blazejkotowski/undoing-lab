3248f960515e30777a85876e1694f74194e5a010
juan <juano.02@hotmail.com>
2024-06-06 08:52:27 +0200

    general updates

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,12 +1,14 @@
-const mainLogo = document.getElementsByClassName("main-logo")[0]
-const sectionWork = document.getElementsByClassName("section-work")[0]
-const worksContainer = document.getElementsByClassName("animation-test")[0]
-const elementTitleDescription = document.getElementsByClassName("element-title-description")
-const headerContainer = document.getElementsByTagName("header")[0]
-const body = document.getElementsByTagName("body")[0]
-const nav = document.getElementsByTagName("nav")[0]
+const mainLogo = document.getElementsByClassName('main-logo')[0]
+const sectionWork = document.getElementsByClassName('section-work')[0]
+const toFullListButton = document.getElementsByClassName('to-full-list-button')[0]
+const fullWorksList = document.getElementsByClassName('full-works-list')[0]
+const worksContainer = document.getElementsByClassName('animation-test')[0]
+const elementTitleDescription = document.getElementsByClassName('element-title-description')
+const headerContainer = document.getElementsByTagName('header')[0]
+const body = document.getElementsByTagName('body')[0]
+const nav = document.getElementsByTagName('nav')[0]
 
-const backgroundSection = document.getElementsByClassName("section-background")[0]
+const backgroundSection = document.getElementsByClassName('section-background')[0]
 
 class Section {
 	top = 0
@@ -34,14 +36,15 @@ class Section {
 	}*/
 }
 
-const aboutSection = new Section(document.getElementsByClassName("section-about")[0])
-const workSection = new Section(document.getElementsByClassName("section-work")[0])
-const contactSection = new Section(document.getElementsByClassName("section-contact")[0])
+const aboutSection = new Section(document.getElementsByClassName('section-about')[0])
+const workSection = new Section(document.getElementsByClassName('section-work')[0])
+const contactSection = new Section(document.getElementsByClassName('section-contact')[0])
 
-window.addEventListener("load", (event) => {
-	aboutSection.element.style.marginTop = `${
+window.addEventListener('load', (event) => {
+	/*aboutSection.element.style.marginTop = `${
 		window.innerHeight - elementTitleDescription[0].clientHeight
-	}px`
+	}px`*/
+	aboutSection.element.style.paddingTop = `${elementTitleDescription[1].clientHeight}px`
 
 	headerContainer.style.height = `${
 		body.getBoundingClientRect().height - (window.innerHeight / 4) * 3
@@ -56,7 +59,7 @@ window.addEventListener("load", (event) => {
 	contactSection.setInitVals()
 
 	mainLogo.addEventListener(
-		"click",
+		'click',
 		() => {
 			window.scrollTo(0, 0)
 		},
@@ -64,20 +67,30 @@ window.addEventListener("load", (event) => {
 	)
 })
 
+if (toFullListButton && fullWorksList) {
+	toFullListButton.addEventListener('click', () => {
+		fullWorksList.classList.toggle('hidden')
+		toFullListButton.classList.toggle('list-visible')
+		headerContainer.style.height = `${
+			body.getBoundingClientRect().height - (window.innerHeight / 4) * 3
+		}px`
+	})
+}
+
 window.onscroll = () => {
 	if (window.pageYOffset > window.innerHeight * 0.3) {
-		mainLogo.classList.remove("main-logo-big")
-		nav.classList.remove("nav-big")
+		mainLogo.classList.remove('main-logo-big')
+		nav.classList.remove('nav-big')
 		nav.style.height = `${mainLogo.clientHeight}px`
 	} else {
-		mainLogo.classList.add("main-logo-big")
-		nav.classList.add("nav-big")
+		mainLogo.classList.add('main-logo-big')
+		nav.classList.add('nav-big')
 	}
 
-	if (window.pageYOffset > window.innerHeight * 0.7) {
-		elementTitleDescription[0].classList.add("small")
+	if (window.pageYOffset > window.innerHeight) {
+		elementTitleDescription[0].classList.add('small')
 	} else {
-		elementTitleDescription[0].classList.remove("small")
+		elementTitleDescription[0].classList.remove('small')
 	}
 	/*
 	if (
@@ -96,40 +109,40 @@ window.onscroll = () => {
 		//console.log("about is visible")
 		for (let i = 0; i < backgroundSection.children.length; i++) {
 			const element = backgroundSection.children[i]
-			element.classList.add("filled-color")
+			element.classList.add('filled-color')
 		}
 	} else {
 		for (let i = 0; i < backgroundSection.children.length; i++) {
 			const element = backgroundSection.children[i]
-			element.classList.remove("filled-color")
+			element.classList.remove('filled-color')
 		}
 	}
 
 	if (window.pageYOffset >= workSection.top && window.pageYOffset < workSection.bottom) {
-		console.log("work is visible")
+		console.log('work is visible')
 		for (let i = 0; i < backgroundSection.children.length; i++) {
 			const element = backgroundSection.children[i]
-			element.classList.add("outlined-black")
+			element.classList.add('outlined-black')
 		}
 	} else {
 		for (let i = 0; i < backgroundSection.children.length; i++) {
 			const element = backgroundSection.children[i]
-			element.classList.remove("outlined-black")
+			element.classList.remove('outlined-black')
 		}
 	}
 
 	if (window.pageYOffset > contactSection.top && window.pageYOffset < contactSection.bottom) {
-		console.log("contact is visible")
+		console.log('contact is visible')
 		for (let i = 0; i < backgroundSection.children.length; i++) {
 			const element = backgroundSection.children[i]
-			element.classList.add("outlined-color")
+			element.classList.add('outlined-color')
 		}
-		mainLogo.classList.add("main-logo-big")
+		mainLogo.classList.add('main-logo-big')
 		nav.style.height = `${navInitHeight}px`
 	} else {
 		for (let i = 0; i < backgroundSection.children.length; i++) {
 			const element = backgroundSection.children[i]
-			element.classList.remove("outlined-color")
+			element.classList.remove('outlined-color')
 		}
 	}
 	/*
